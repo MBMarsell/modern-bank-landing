7dd5ce6b72ac08635f57072831598271b9d50a71
MBMarsell <martbustmar@gmail.com>
2022-08-22 17:26:33 +0200

    add stats section

--- src/constants/index.js
+++ src/constants/index.js
@@ -88,18 +88,18 @@ export const feedback = [
 export const stats = [
   {
     id: 'stats-1',
-    title: 'User Active',
-    value: '3800+',
+    title: 'Active Users',
+    value: '38000+',
   },
   {
     id: 'stats-2',
     title: 'Trusted by Company',
-    value: '230+',
+    value: '430+',
   },
   {
     id: 'stats-3',
-    title: 'Transaction',
-    value: '$230M+',
+    title: 'Transactions',
+    value: '$830M+',
   },
 ];
 
