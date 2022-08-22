e212df82f6bb71c34561e748b58e804fc5469eb9
MBMarsell <martbustmar@gmail.com>
2022-08-22 17:46:54 +0200

    add button w animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </motion.button>
+  );
 };
 
 export default Button;
