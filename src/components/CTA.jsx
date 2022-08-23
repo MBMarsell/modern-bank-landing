72406f1d811b15f852f887fb91bd9538da57632c
MBMarsell <martbustmar@gmail.com>
2022-08-23 14:46:59 +0200

    finish CTA comp

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import styles from '../style';
+import Button from './Button';
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Try out our service now!</h2>
+      <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+        A solution for your business to accept card payments and grow your
+        business anywhere on the globe.
+      </p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
 
 export default CTA;
